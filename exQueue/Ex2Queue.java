7f75dbc54f74e01837baa94a84acf175a1590a1f
Gabriel <gabriel.assis2309@gmail.com>
2023-09-05 15:57:35 -0300

    Exercícios de Queue/Stack

--- exQueue/Ex2Queue.java
+++ exQueue/Ex2Queue.java
@@ -31,7 +31,7 @@ public class Ex2Queue {
                         System.out.println("A fila está vazia!");
                     } else {
                         livros.pop();
-                        System.out.println("Cliente retirado da  fila!");
+                        System.out.println("Livro retirado da pilha!");
                     }
                 }
                 case 0 -> System.out.println("Saiu!");
